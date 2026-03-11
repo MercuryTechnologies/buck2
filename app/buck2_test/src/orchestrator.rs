0477c4a631655271801afb4a18899c70080a5cdc
Joseph Sumabat <joseph@mercury.com>
2026-03-11 02:03:27 -0400

    test caching dice patch
    Avoids re-running passed tests. Only compatible with DICE (local only).

--- app/buck2_test/src/orchestrator.rs
+++ app/buck2_test/src/orchestrator.rs
@@ -565,14 +565,14 @@ struct TestExecutionKey {
 #[derive(Clone, Dupe, Debug, Eq, Hash, PartialEq, Allocative, Pagable)]
 enum TestExecutionPrefix {
     Listing,
-    Testing(Arc<ForwardRelativePathBuf>),
+    Testing,
 }
 
 impl TestExecutionPrefix {
-    fn new(stage: &TestStage, session: &TestSession) -> Self {
+    fn new(stage: &TestStage, _session: &TestSession) -> Self {
         match stage {
             TestStage::Listing { .. } => TestExecutionPrefix::Listing,
-            TestStage::Testing { .. } => TestExecutionPrefix::Testing(session.prefix().dupe()),
+            TestStage::Testing { .. } => TestExecutionPrefix::Testing,
         }
     }
 }
@@ -581,7 +581,7 @@ impl Display for TestExecutionPrefix {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             TestExecutionPrefix::Listing => write!(f, "Listing"),
-            TestExecutionPrefix::Testing(prefix) => write!(f, "Testing({prefix})"),
+            TestExecutionPrefix::Testing => write!(f, "Testing"),
         }
     }
 }
@@ -631,7 +631,7 @@ async fn prepare_and_execute(
 ) -> Result<ExecuteData, ExecuteError> {
     let execute_on_dice = match key.stage.as_ref() {
         TestStage::Listing { cacheable, .. } => *cacheable,
-        TestStage::Testing { .. } => false,
+        TestStage::Testing { .. } => true,
     };
     if execute_on_dice {
         let result = tokio::select! {
