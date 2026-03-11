7767410fc9edc5457f18fa56f8f360711944a0db
Joseph Sumabat <joseph@mercury.com>
2026-03-11 07:56:09 -0400

    Add execution_concurrency_limit to RE client https://github.com/facebook/buck2/pull/1219

--- app/buck2_re_configuration/src/lib.rs
+++ app/buck2_re_configuration/src/lib.rs
@@ -26,6 +26,7 @@ const DEFAULT_MAX_RETRIES: usize = 5;
 pub trait RemoteExecutionStaticMetadataImpl: Sized {
     fn from_legacy_config(legacy_config: &LegacyBuckConfig) -> buck2_error::Result<Self>;
     fn cas_semaphore_size(&self) -> usize;
+    fn exec_semaphore_size(&self) -> usize;
 }
 
 #[derive(Clone, Debug, Allocative)]
@@ -376,6 +377,10 @@ mod fbcode {
         fn cas_semaphore_size(&self) -> usize {
             self.cas_connection_count as usize * 30
         }
+
+        fn exec_semaphore_size(&self) -> usize {
+            self.execution_concurrency_limit as usize
+        }
     }
 }
 
@@ -397,6 +402,10 @@ mod not_fbcode {
         fn cas_semaphore_size(&self) -> usize {
             // FIXME: make this configurable?
             1024
+        }
+
+        fn exec_semaphore_size(&self) -> usize {
+            self.0.execution_concurrency_limit.unwrap_or(400)
         }
     }
 }
