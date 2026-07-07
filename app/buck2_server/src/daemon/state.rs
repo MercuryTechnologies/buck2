980f64649e5344379fc2478b7c588a1ee98eb9f5
Rebecca Turner <rbt@sent.as>
2026-07-07 17:33:24 -0400

    GHA: Fix CI (#21)

--- app/buck2_server/src/daemon/state.rs
+++ app/buck2_server/src/daemon/state.rs
@@ -54,7 +54,6 @@ use buck2_execute::dep_file_state::DEP_FILE_STORE;
 use buck2_execute::digest_config::DigestConfig;
 use buck2_execute::execute::blocking::BlockingExecutor;
 use buck2_execute::execute::blocking::BuckBlockingExecutor;
-use buck2_execute::execute::blocking::DirectIoExecutor;
 use buck2_execute::materialize::materializer::MaterializationMethod;
 use buck2_execute::materialize::materializer::Materializer;
 use buck2_execute::re::manager::ReConnectionManager;
